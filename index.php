831e05bdaa564cfceaea0d975b432b222a836dad
Amanda <amanda.k.odonnell@gmail.com>
2019-02-09 18:20:09 -0500

    first pass through the pages
    still need to improve the index and put actual content in Joseph's page

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lab 3 | Team Boston</title>
+</head>
+<body>
+    <h1>9 Very Loosely Linked Topics</h1>
+    <a href="amanda.php">Sustenance</a>
+    <a href="jacob.php">Cat Breeds</a>
+    <a href="joseph.php">WHATEVER JOSEPH PICKED -- CHANGE THIS</a>
+</body>
+</html>
---
+++ joseph.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CHANGE ONCE KNOW TOPIC</title>
+</head>
+<body>
+<h1>OVERALL TOPIC</h1>
+    <p>DESCRIPTON OF IA FLOW -- NECESSARY TO INCLUDE?</p>
+    <h2>TOPIC 1<h2>
+    <h3>SUBHEADING 1</h3>
+    <h3>SUBHEADING 2</h3>
+    <h3>SUBHEADING 3</h3>
+    <h2>TOPIC 2<h2>
+    <h3>SUBHEADING 1</h3>
+    <h3>SUBHEADING 2</h3>
+    <h3>SUBHEADING 3</h3>
+    <h2>TOPIC 3<h2>
+    <h3>SUBHEADING 1</h3>
+    <h3>SUBHEADING 2</h3>
+    <h3>SUBHEADING 3</h3>
+
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="amanda.php">Sustenance</a>
+        <a href="jacob.php">Cat Breeds</a>
+        <a href="joseph.php">WHATEVER JOSEPH PICKED -- CHANGE THIS</a>
+    </nav>
+</body>
+</html>
